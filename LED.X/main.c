626b655290b7d6c1735ed4f393047b3cbcddd59f
Talash Ganvir <talash.ganvir@ppp.net>
2019-12-03 11:57:26 +0530

    led continuesoly on

---
+++ LED.X/main.c
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.c
+ * Author: talash.ganvir
+ *
+ * Created on 3 December, 2019, 11:45 AM
+ */
+#include<xc.h>
+#pragma config XINST = OFF      // Extended Instruction Set (Disabled)
+// CONFIG1H
+#pragma config FOSC = HS1       // Oscillator (HS oscillator (Medium power, 4 MHz - 16 MHz))
+// CONFIG2H
+#pragma config WDTEN = OFF      // Watchdog Timer (WDT disabled in hardware; SWDTEN bit disabled) 
+
+void main()
+{
+    TRISD = 0;
+    LATD = 0;
+
+    while (1)
+    {
+        LATD = 1;
+
+    }
+}
